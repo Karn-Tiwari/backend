90c9ec3621d288de29ab6aed091d63b6d5640442
Karn Tiwari <karntiwari5353@gmail.com>
2024-03-28 17:07:12 +0530

    connection established to mongodb

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({ path: "./env" });
+
+connectDB();
+
+// // Connect to MongoDB ye ek iffi function jo immediately execute hota hai
+// // ;(async () => {})();
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("Error: ", error);
+//       throw error;
+//     });
+//     app.listen(3000, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("Error: ", error);
+//     throw error;
+//   }
+// })();
