0b3f45462c5e4407f00a930163706df326ee173c
Karn Tiwari <karntiwari5353@gmail.com>
2024-07-30 17:02:22 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -64,25 +64,29 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 };
 
 userSchema.methods.generateAccessToken = function () {
-  jwt.sign({
-    _id: this._id,
-    email: this.email,
-    username: this.username,
-    fullname: this.fullname,
-  }),
+  return (
+    jwt.sign({
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    }),
     process.env.ACCESS_TOKEN_SECRET,
     {
       expireIn: process.env.ACCESS_TOKEN_EXPIRY,
-    };
+    }
+  );
 };
 
-userSchema.methods.generateRefereshToken = function () {
-  jwt.sign({
-    _id: this._id,
-  }),
-    process.env.REFERSH_TOKEN_SECRET,
+userSchema.methods.generateRefreshToken = function () {
+  return (
+    jwt.sign({
+      _id: this._id,
+    }),
+    process.env.REFRESH_TOKEN_SECRET,
     {
-      expireIn: process.env.REFERESH_TOKEN_EXPIRY,
-    };
+      expireIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
 };
 export const User = mongoose.model("User", userSchema);
